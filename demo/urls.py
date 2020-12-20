e85ca0a95b65e9aa4bd575d3b565ab46b29164e5
Manoj Pravin <smanojpravin@gmail.com>
2020-12-21 03:05:49 +0530

    3  commit

--- demo/urls.py
+++ demo/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/<int:id>/', store.views.show, name='show-product'),
     path('cart/', store.views.cart, name='shopping-cart'),
-    path('', store.views.index, name='list-products'),
-    path('home', store.views.home, name='home'),
+    # path('', store.views.index, name='list-products'),
+    path('', store.views.home, name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
