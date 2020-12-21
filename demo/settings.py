e5e346b5e62060670e58005a5818cfa92d6d5ec4
Manoj Pravin <smanojpravin@gmail.com>
2020-12-21 14:59:05 +0530

    Ne commit css

--- demo/settings.py
+++ demo/settings.py
@@ -107,7 +107,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles'))
 
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'store', 'uploads'))
 MEDIA_URL = '/uploads/'
