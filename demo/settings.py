6317e8a66020a36cf5339dfbebf5e493d0f5bf36
Manoj Pravin <smanojpravin@gmail.com>
2020-12-21 15:13:14 +0530

    commit again for white noise

--- demo/settings.py
+++ demo/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
